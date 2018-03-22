1166e6b815c4557651433e32bd17f2cfa0c18485
delbertbeta <delbertbeta@live.com>
2018-03-22 16:20:55 +0800

    :disappointed:Added mask animation.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
             item.classList.remove('hide');
         })
     }
+
     var initAnimation = function () {
         var coverTitle = document.getElementById('coverTitle');
         var titleContent = coverTitle.attributes.content.value;
@@ -24,7 +25,28 @@
         }, 150);
     }
 
+    var initParallax = function () {
+        var cover = document.getElementById('cover');
+        var mask = document.getElementById('mask');
+        var adBackground = document.getElementById('adBackground');
+        var animationFrame;
+        var perFrameCallback = function () {
+            var top = window.scrollY;
+            var total = document.body.clientHeight;
+            cover.style.bottom = top + 'px';
+            mask.style.opacity = 1 - top / total;
+            if (window.scrollY === document.body.clientHeight) {
+                cover.classList.remove('shadow');
+            } else {
+                cover.classList.add('shadow');
+            }
+            animationFrame = requestAnimationFrame(perFrameCallback);
+        }
+        animationFrame = requestAnimationFrame(perFrameCallback);
+    }
+
     window.addEventListener('load', function () {
         initAnimation();
+        initParallax();
     });
 })();
